5335695bddd1928c643d07a40dd8bed94bc6c736
Alsnj20 <marieljara656@gmail.com>
2024-07-11 11:57:30 -0500

    Prueba de crear un profesor

--- backend/system/urls.py
+++ backend/system/urls.py
@@ -2,9 +2,13 @@ from django.urls import include, path
 from .import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
-    path("system/user/register/", views.CreateUserView.as_view(), name="register"),
-    path('system/user/create-user/', views.CreateUserView.as_view(), name='create-user'),
-    path("system/token/", TokenObtainPairView.as_view(), name="get_token"),
-    path("system/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
-    path("system-auth/", include("rest_framework.urls", namespace="rest_framework")) 
+  # Registro de usuarios
+  path("system/user/register/", views.CreateUserView.as_view(), name="register"),
+  path('system/user/create-user/', views.CreateUserView.as_view(), name='create-user'),
+  # Tokens de autenticación
+  # Token => Existe una cuenta
+  path("system/token/", TokenObtainPairView.as_view(), name="get_token"),
+  path("system/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+  # Usuarios
+  path("system-auth/", include("rest_framework.urls", namespace="rest_framework")) 
 ]
