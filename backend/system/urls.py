18bee0527e4b1b121ae09202d12845ef5f3ef96e
Alsnj20 <marieljara656@gmail.com>
2024-07-11 15:19:15 -0500

    Register y Login para usuarios

--- backend/system/urls.py
+++ backend/system/urls.py
@@ -4,11 +4,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
   # Registro de usuarios
   path("system/user/register/", views.CreateUserView.as_view(), name="register"),
-  path('system/user/create-user/', views.CreateUserView.as_view(), name='create-user'),
   # Tokens de autenticación
   # Token => Existe una cuenta
   path("system/token/", TokenObtainPairView.as_view(), name="get_token"),
   path("system/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
   # Usuarios
-  path("system-auth/", include("rest_framework.urls", namespace="rest_framework")) 
+  path("system-auth/", include("rest_framework.urls", namespace="rest_framework")), 
+  # Login
+  path("system/user/login/", views.LoginView.as_view(), name="login"),
 ]
--- backend/system/views.py
+++ backend/system/views.py
@@ -1,14 +1,33 @@
+# Register
 from rest_framework import generics, permissions
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import *
 from .serializers import *
+# Login
+from django.contrib.auth import authenticate, login
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status 
 
 class CreateUserView(generics.CreateAPIView):
   queryset = Usuario.objects.all()
   serializer_class = UsuarioSerializer
-  permission_classes = [AllowAny]
+  permission_classes = [permissions.AllowAny]
 
   def get_serializer_context(self):
     context = super().get_serializer_context()
     context.update({"request": self.request})
     return context
+  
+class LoginView(APIView):
+  permission_classes = [permissions.AllowAny]
+
+  def post(self, request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    user = authenticate(request, email=email, password=password)
+    if user is not None:
+      login(request, user)
+      return Response({"message": "Usuario autenticado", "user": UsuarioSerializer(user).data})
+    else:
+      return Response({"message": "Usuario no autenticado"}, status=status.HTTP_401_UNAUTHORIZED)
