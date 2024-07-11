ea492ef4409b7930b6251dc7dd2f97b3abb13a3c
Alsnj20 <marieljara656@gmail.com>
2024-07-11 18:46:58 -0500

    Configuración de los routers

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,30 +1,26 @@
-import { useState, useEffect } from 'react'
-import axiosInstance from './axiosConfig'
-import './App.css'
+import axios from 'axios'
+//Home
+import HomeAdmin from './components/HomeAdmin'
+import HomeTeacher from './components/HomeTeacher'
+import HomeStudent from './components/HomeStudent'
+//Register
+import RegisterUser from './components/RegisterUser'
+import LoginUser from './components/LoginUser'
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  const[users, setUsers] = useState([])
-
-  useEffect(() => {
-    axiosInstance.get('users/')
-      .then((response) => {
-        setUsers(response.data)
-      })
-      .catch((error) => {
-        console.log('Error fetch',error)
-      })
-  }, [])
-
   return (
-    <div className="App">
-      <h1>Usuarios</h1>
-      <ul>
-        {users.map(usuario => (
-          <li key={usuario.id}>{usuario.name} ({usuario.email})</li>
-        ))}
-      </ul>
-    </div>
-  );  
+    <Router>
+      <Routes>
+        <Route path="/home" element={<HomeAdmin />} />
+        <Route path="/teacher" element={<HomeTeacher />} />
+        <Route path="/student" element={<HomeStudent />} />
+        <Route path="/" element={<RegisterUser />} />
+        <Route path="/login" element={<LoginUser />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
